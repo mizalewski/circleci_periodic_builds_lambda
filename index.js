4cd049fce5f892b39f8acc168ab18b7ea5cfb182
Michał Zalewski <michal@mzalewski.net>
2018-03-22 23:02:01 +0100

    Lambda script for nightly build schedule

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const https = require('https')
+
+const buildOptions = (event) => {
+  const {vcsType, project, branch, circleToken} = event
+
+  return {
+    hostname: 'circleci.com',
+    port: 443,
+    path: `/api/v1.1/project/${vcsType}/${project}/tree/${branch}?circle-token=${circleToken}`,
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'cache-control': 'no-cache'
+    }
+  }
+}
+
+const postContent = JSON.stringify({
+  build_parameters: {
+    RUN_NIGHTLY_BUILD: true
+  }
+})
+
+exports.handler = (event, context, callback) => {
+  const options = buildOptions(event)
+  console.log(options)
+  const req = https.request(options, (res) => {
+    let body = ''
+
+    console.log('statusCode:', res.statusCode)
+    console.log('headers:', res.headers)
+
+    if (res.statusCode !== 201) {
+      context.fail('Build schedule failed')
+      callback('error', res.statusCode)
+    }
+
+    res.on('data', (chunk) => body += chunk)
+    res.on('end', () => {
+      console.log('Successfully processed HTTPS response')
+      context.succeed()
+      callback(null, body)
+    })
+  })
+  req.on('error', callback)
+  req.write(postContent)
+  req.end()
+}
